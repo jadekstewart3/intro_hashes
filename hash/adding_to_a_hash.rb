9652c5e9884abcaa652c6119368091e7468ee06e
Dani Coleman <danicoleman00@gmail.com>
2021-12-02 11:11:17 -0700

    Add adding_to_a_hash.rb with exercise

---
+++ hash/adding_to_a_hash.rb
@@ -0,0 +1,12 @@
+suitcase = {
+  "socks" => 4,
+  "pants" => 1
+}
+
+# How can we add the following key/value pairs:
+# 1. 2 jackets
+# 2. 4 shirts
+# 3. 2 shoes
+
+# How can we increase the existing value:
+# 1. Change pants to have a value of 2

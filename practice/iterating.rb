6de79e665157f6f266bb5fe6234c9415dd203b23
Brian Zanti <brianzanti@gmail.com>
2022-10-19 10:46:17 -0500

    adds iterating practice

---
+++ practice/iterating.rb
@@ -0,0 +1,10 @@
+suitcase = {
+  "socks" => 4,
+  "pants" => 2,
+  "jackets" => 2,
+  "shirts" => 4,
+  "shoes" => 2
+}
+
+# Use .each to print out each key value pair
+# in the format "I am bringing <quantity> <item> on my trip"
